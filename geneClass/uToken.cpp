182d24d864bd3c6f4acf760aaac991e7351eb0ce
Charles Joly Beauparlant <charles.joly-beauparlant@crchul.ulaval.ca>
2012-08-16 15:53:39 -0400

    First version that compile

--- geneClass/uToken.cpp
+++ geneClass/uToken.cpp
@@ -2,16 +2,74 @@
 
 //enum class token_param { CHR, START_POS, END_POS, STRAND, MAP_SCORE, PHRED_SCORE, CIGAR, SEQUENCE, SEQ_NAME, FLAGS };
 
-void uToken::setParam(token_param::token_param& name, const std::string& value) {
+/* \brief uToken constructor.
+ * \param istream& paramList: a stream containing all the parameters in the format: <token_param>\t<value>\n<token_param>\t<value>\n...
+ */
+uToken::uToken(std::istream& paramList){
+	char line[1024];	
+	while (paramList.getline(line, 1024)) {
+		std::stringstream ss;
+		ss << line;
+		/**< Fetch name */
+		/**< Fetch value */
+	}
+
+}
+
+/* \brief Fetch a param. Throw param_not_found if the param does not exist.
+ * \param token_param& name: the name of the param we wish to get.
+ */
+std::string uToken::getParam(token_param& name) const {
+
+}
+
+/* \brief Set a param only if it's format is valid
+ * \param token_param& name: type of param
+ * \param const std::string& value: the value of the parameter
+ */
+void uToken::_setParam(token_param& name, std::string& value) {
 	// TODO: What should we do in case the name is already setted? Overwrite silently, warning or error?
-	_validateParam(name, value);
-	m_params(name) = value;
+	try {
+		_validateParam(name, value);
+	}
+	catch (invalid_value_error& e) {
+		throw e;		
+	}
+	m_params.insert(make_pair(name, value));
 }
 
-std::string uToken::getParam(token_param::token_param& name) const {
-
-}
-
-void uToken::_validateParam(token_param::token_param& name, const std::string& value) {
-	
+/* \brief Validate the parameter. Some tests are general, other are specific for each parameter.
+ * \param token_param& name: type of param
+ * \param const std::string& value: the value of the parameter
+ */
+void uToken::_validateParam(token_param& name, const std::string& value) {
+	/**< In every case, an empty string is an invalid value */
+	if (value.size() == 0) {
+		invalid_value_throw e;
+		e << invalid_value_error(value);
+		throw e;
+	}
+	/**< Then we do basic check for every type of parameters */
+	switch(name) {
+	case token_param::CHR:
+		break;
+	case token_param::START_POS:
+		break;
+	case token_param::END_POS:
+		break;
+	case token_param::STRAND:
+		break;
+	case token_param::MAP_SCORE:
+		break;
+	case token_param::PHRED_SCORE:
+		break;
+	case token_param::CIGAR:
+		break;
+	case token_param::SEQUENCE:
+		break;
+	case token_param::SEQ_NAME:
+		break;
+	case token_param::FLAGS:
+		break;
+	}
 }
--- geneClass/uToken.h
+++ geneClass/uToken.h
@@ -1,6 +1,7 @@
 #ifndef UTOKEN_H_INCLUDED
 #define UTOKEN_H_INCLUDED
 
+#include <iostream>
 #include <map>
 #include <string>
 #include <sstream>
@@ -16,38 +17,52 @@ enum class token_param { CHR, START_POS, END_POS, STRAND, MAP_SCORE, PHRED_SCORE
 /**< All the data is saved in a map in string format */
 class uToken {
 public:
-	uToken(istream& paramList);
-	std::string getParam(token_param::token_param& name) const;
+	uToken(std::istream& paramList);
+	std::string getParam(token_param& name) const;
+//	friend std::ostream& operator<<(std::ostream& Str, token_param name);
 
 private:
 	std::map<token_param, std::string> m_params;
-	void _setParam(token_param::token_param& name, const std::string& value);
-	void _validateParam(token_param::token_param& name, const std::string& value);
-	bool _chrIsValid(const std::string& name) const;
-	bool _posIsValid(const std::string& name) const;
-	bool _strandIsValid(const std::string& name) const;
+	void _setParam(token_param& name, std::string& value);
+	void _validateParam(token_param& name, const std::string& value);
+	bool _chrIsValid(const std::string& name) const { return true; };
+	bool _posIsValid(const std::string& name) const { return true; };
+	bool _strandIsValid(const std::string& name) const { return true; };
+	bool _mapScoreIsValid(const std::string& name) const { return true; };
+	bool _phredScoreIsValid(const std::string& name) const { return true; };
 
-} // End of class Token
+}; // End of class Token
 
 /**< Overloading of stream operator for token_param */
-inline std::ostream& operator<<(std::ostream & Str, token_param::token_param name) {
+inline std::ostream & operator<<(std::ostream& Str, token_param name) {
 	switch (name) {
-	case CHR: return Str << "CHR";
-	case START_POS: return Str << "START_POS";
-	case END_POS: return Str << "END_POS";
-	case STRAND: return Str << "STRAND";
-	case MAP_SCORE: return Str << "MAP_SCORE";
-	case PHRED_SCORE: return Str << "PHRED_SCORE";
-	case CIGAR: return Str << "CIGAR";
-	case SEQUENCE: return Str << "SEQUENCE";
-	case SEQ_NAME: return Str << "SEQ_NAME";
-	case FLAGS: return Str << "FLAGS";
+	case token_param::CHR: return Str << "CHR";
+	case token_param::START_POS: return Str << "START_POS";
+	case token_param::END_POS: return Str << "END_POS";
+	case token_param::STRAND: return Str << "STRAND";
+	case token_param::MAP_SCORE: return Str << "MAP_SCORE";
+	case token_param::PHRED_SCORE: return Str << "PHRED_SCORE";
+	case token_param::CIGAR: return Str << "CIGAR";
+	case token_param::SEQUENCE: return Str << "SEQUENCE";
+	case token_param::SEQ_NAME: return Str << "SEQ_NAME";
+	case token_param::FLAGS: return Str << "FLAGS";
 	default: return Str << (int) name;
 	}
 }
 
-inline istream& operator >>(istream &is, token_param::token_param& name) {
-	is >> name;
+inline std::istream& operator>>(std::istream &is, token_param& name) {
+	std::string token;
+	is >> token;
+	if (token == "CHR") name = token_param::CHR;
+	else if (token == "START_POS") name = token_param::START_POS;
+	else if (token == "END_POS") name = token_param::END_POS;
+	else if (token == "STRAND") name = token_param::STRAND;
+	else if (token == "MAP_SCORE") name = token_param::MAP_SCORE;
+	else if (token == "PHRED_SCORE") name = token_param::PHRED_SCORE;
+	else if (token == "CIGAR") name = token_param::CIGAR;
+	else if (token == "SEQUENCE") name = token_param::SEQUENCE;
+	else if (token == "SEQ_NAME") name = token_param::SEQ_NAME;
+	else if (token == "FLAGS") name = token_param::FLAGS;
 	return is;
 }
 
