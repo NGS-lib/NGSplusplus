06e6de67ca601dc2fc8246996bedb78e4243daad
Charles Joly Beauparlant <charles.joly-beauparlant@crchul.ulaval.ca>
2012-08-16 14:49:11 -0400

    Added overloading of stream operator for token_param enum class

--- geneClass/uToken.h
+++ geneClass/uToken.h
@@ -3,6 +3,7 @@
 
 #include <map>
 #include <string>
+#include <sstream>
 #include <stdexcept>
 #include "boost/exception/all.hpp"
 
@@ -15,20 +16,44 @@ enum class token_param { CHR, START_POS, END_POS, STRAND, MAP_SCORE, PHRED_SCORE
 /**< All the data is saved in a map in string format */
 class uToken {
 public:
-	uToken() { };
-	void setParam(token_param::token_param& name, const std::string& value);
+	uToken(istream& paramList);
 	std::string getParam(token_param::token_param& name) const;
 
 private:
 	std::map<token_param, std::string> m_params;
+	void _setParam(token_param::token_param& name, const std::string& value);
 	void _validateParam(token_param::token_param& name, const std::string& value);
+	bool _chrIsValid(const std::string& name) const;
+	bool _posIsValid(const std::string& name) const;
+	bool _strandIsValid(const std::string& name) const;
 
 } // End of class Token
 
+/**< Overloading of stream operator for token_param */
+inline std::ostream& operator<<(std::ostream & Str, token_param::token_param name) {
+	switch (name) {
+	case CHR: return Str << "CHR";
+	case START_POS: return Str << "START_POS";
+	case END_POS: return Str << "END_POS";
+	case STRAND: return Str << "STRAND";
+	case MAP_SCORE: return Str << "MAP_SCORE";
+	case PHRED_SCORE: return Str << "PHRED_SCORE";
+	case CIGAR: return Str << "CIGAR";
+	case SEQUENCE: return Str << "SEQUENCE";
+	case SEQ_NAME: return Str << "SEQ_NAME";
+	case FLAGS: return Str << "FLAGS";
+	default: return Str << (int) name;
+	}
+}
 
-// uToken exceptions
+inline istream& operator >>(istream &is, token_param::token_param& name) {
+	is >> name;
+	return is;
+}
+
+/**<  uToken exceptions */
 struct uToken_exception_base : virtual std::exception, virtual boost::exception {};
-struct value_throw : virtual uToken_exception_base{};
+struct invalid_value_throw : virtual uToken_exception_base{};
 typedef boost::error_info<struct invalid_param_info, std::string> invalid_value_error;
 
 #endif // UTOKEN_H_INCLUDED
