39725726da39d2a20e5853074c307139a5e99989
Charles Joly Beauparlant <charles.joly-beauparlant@crchul.ulaval.ca>
2012-09-12 14:43:58 -0400

    Changed uToken.h and uHeader.h includes

---
+++ geneClass/NGS++.h
@@ -0,0 +1,20 @@
+/**< All the includes */
+#include "utility/utility.h"
+
+#include "IO/uToken.h"
+#include "IO/uHeader.h"               
+
+#include "uFormatChrom.h"          
+#include "uFunctions.h"            
+#include "uGeneRef.h"              
+#include "uFormatExperiment.h"     
+#include "uGene.h"                 
+#include "uRegion.h"
+#include "uFormatBase.h"           
+#include "uFormats.h"              
+#include "uGeneException.h"        
+#include "uParser.h"               
+#include "uTags.h"
+
+//#include "uParser2.h"              
+//#include "cluster.h"               
