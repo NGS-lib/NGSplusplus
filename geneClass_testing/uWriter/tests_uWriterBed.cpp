d7ea56875c59ad8d1344e69407b2d7b101c627cf
Charles Joly Beauparlant <charles.joly-beauparlant@crchul.ulaval.ca>
2012-09-18 15:08:29 -0400

    There is now a distinction between Bed4 and Bed6

--- geneClass_testing/uWriter/tests_uWriterBed.cpp
+++ geneClass_testing/uWriter/tests_uWriterBed.cpp
@@ -11,28 +11,40 @@ using namespace NGS;
  * Tests for the function: 
  *		void writeToken(const uToken& token);
  *	Valid case:
- *		SixValidValues
- *		NoScoreValue
- *		NoNameValue
- *		NoScoreStrandValues
+ *		FourValidValuesBed4
+ *		SixValidValuesBed6
+ *		NoScoreValueBed6
+ *		NoNameValueBed4
+ *		NoNameValueBed6
+ *		NoScoreStrandValuesBed6
  */
-TEST_F(TestsBedWriter, WriteToken_SixValidValues) {
-	string expected = "chr1\t1\t21\tab00001\t111\t+\n";
-	ASSERT_TRUE(m_pOss->str().find(expected) != string::npos);
-}
-
-TEST_F(TestsBedWriter, WriteToken_NoScoreValue) {
-	string expected = "chr1\t1\t21\tab00001\t.\t+\n";
-	ASSERT_TRUE(m_pOss->str().find(expected) != string::npos);
-}
-
-TEST_F(TestsBedWriter, WriteToken_NoNameValue) {
-	string expected = "chr1\t1\t21\t.\t111\t+\n";
-	ASSERT_TRUE(m_pOss->str().find(expected) != string::npos);
-}
-
-TEST_F(TestsBedWriter, WriteToken_NoScoreStrandValues) {
+TEST_F(TestsBedWriter, WriteToken_FourValidValuesBed4) {
 	string expected = "chr1\t1\t21\tab00001\n";
-	ASSERT_TRUE(m_pOss->str().find(expected) != string::npos);
+	ASSERT_TRUE(m_pOssBed4->str().find(expected) != string::npos);
+}
+
+TEST_F(TestsBedWriter, WriteToken_SixValidValuesBed6) {
+	string expected = "chr1\t1\t21\tab00001\t111\t+\n";
+	ASSERT_TRUE(m_pOssBed6->str().find(expected) != string::npos);
+}
+
+TEST_F(TestsBedWriter, WriteToken_NoScoreValueBed6) {
+	string expected = "chr1\t1\t21\tab00001\t.\t+\n";
+	ASSERT_TRUE(m_pOssBed6->str().find(expected) != string::npos);
+}
+
+TEST_F(TestsBedWriter, WriteToken_NoNameValueBed4) {
+	string expected = "chr1\t1\t21\t.\n";
+	ASSERT_TRUE(m_pOssBed4->str().find(expected) != string::npos);
+}
+
+TEST_F(TestsBedWriter, WriteToken_NoNameValueBed6) {
+	string expected = "chr1\t1\t21\t.\t111\t+\n";
+	ASSERT_TRUE(m_pOssBed6->str().find(expected) != string::npos);
+}
+
+TEST_F(TestsBedWriter, WriteToken_NoScoreStrandValuesBed6) {
+	string expected = "chr1\t1\t21\tab00001\t.\t.\n";
+	ASSERT_TRUE(m_pOssBed6->str().find(expected) != string::npos);
 }
 
