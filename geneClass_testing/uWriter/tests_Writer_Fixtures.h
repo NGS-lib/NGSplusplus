eeeb714378730ab64fb2752a960076e902a3f8ae
Charles Joly Beauparlant <charles.joly-beauparlant@crchul.ulaval.ca>
2012-09-18 15:08:03 -0400

    Splitted token creation in another class and there is now a distinction between Bed4 and Bed6

--- geneClass_testing/uWriter/tests_Writer_Fixtures.h
+++ geneClass_testing/uWriter/tests_Writer_Fixtures.h
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <vector>
 #include <string>
 #include <sstream>
 #include "IO/Writer/uWriter.h"
@@ -6,52 +7,70 @@
 using namespace std;
 using namespace NGS;
 
-class TestsBedWriter: public ::testing::Test {
+class validTokens {
 public:
-	TestsBedWriter() {
-		// Initialize write
-		m_pOss = new ostringstream(ostringstream::out);	
-		m_pWriter = new uWriter(m_pOss, "BED");
-		// Write a token with writer
+	validTokens() {
+		/**< Token 1: Contain all token_param */
 		stringstream ss;
 		ss << "CHR\tchr1\n" << "START_POS\t1\n" << "END_POS\t21\n";
 		ss << "STRAND\t+\n" << "MAP_SCORE\t255\n" << "PHRED_SCORE\t####################\n";
 		ss << "CIGAR\t2M3I2X1=12X\n" << "SEQUENCE\tACGTN.acgtn.ACGTGTCN\n";
 		ss << "SEQ_NAME\tab00001\n" << "SCORE\t111\n" << "FLAGS\t256\n";
 		uToken Token1(ss);
-		m_pWriter->writeToken(Token1);
-		// Write a second token with writer
+		m_vTokens.push_back(Token1);
+		/**< Token 2: Token 1 minus SCORE */
 		ss.clear();
 		ss << "CHR\tchr1\n" << "START_POS\t1\n" << "END_POS\t21\n";
 		ss << "STRAND\t+\n" << "MAP_SCORE\t255\n" << "PHRED_SCORE\t####################\n";
 		ss << "CIGAR\t2M3I2X1=12X\n" << "SEQUENCE\tACGTN.acgtn.ACGTGTCN\n";
 		ss << "SEQ_NAME\tab00001\n" << "FLAGS\t256\n";
 		uToken Token2(ss);
-		m_pWriter->writeToken(Token2);
-		// Write a third token with writer
+		m_vTokens.push_back(Token2);
+		/**< Token 3: Token 1 minus SEQ_NAME */
 		ss.clear();
 		ss << "CHR\tchr1\n" << "START_POS\t1\n" << "END_POS\t21\n";
 		ss << "STRAND\t+\n" << "MAP_SCORE\t255\n" << "PHRED_SCORE\t####################\n";
 		ss << "CIGAR\t2M3I2X1=12X\n" << "SEQUENCE\tACGTN.acgtn.ACGTGTCN\n";
 		ss << "SCORE\t111\n" << "FLAGS\t256\n";
 		uToken Token3(ss);
-		m_pWriter->writeToken(Token3);
-		// Write a fourth token with writer
+		m_vTokens.push_back(Token3);
+		/**< Token 4: Token 1 minus SCORE and STRAND */
 		ss.clear();
 		ss << "CHR\tchr1\n" << "START_POS\t1\n" << "END_POS\t21\n";
 		ss << "MAP_SCORE\t255\n" << "PHRED_SCORE\t####################\n";
 		ss << "CIGAR\t2M3I2X1=12X\n" << "SEQUENCE\tACGTN.acgtn.ACGTGTCN\n";
 		ss << "SEQ_NAME\tab00001\n" << "FLAGS\t256\n";
 		uToken Token4(ss);
-		m_pWriter->writeToken(Token4);
+		m_vTokens.push_back(Token4);
+	}
 
+	vector<uToken> m_vTokens;
+};
+
+class TestsBedWriter: public ::testing::Test {
+public:
+	TestsBedWriter() {
+		/**< Initialize streams */
+		m_pOssBed4 = new ostringstream(ostringstream::out);	
+		m_pOssBed6 = new ostringstream(ostringstream::out);	
+		/**< Initialize writers */
+		uWriter writerBed4(m_pOssBed4, "BED4");
+		uWriter writerBed6(m_pOssBed6, "BED6");
+		/**< Write tokens */
+		validTokens tokens;
+		for (size_t i = 0; i < tokens.m_vTokens.size(); i++) {
+			writerBed4.writeToken(tokens.m_vTokens[i]);
+			writerBed6.writeToken(tokens.m_vTokens[i]);
+		}
 	}
+
 	~TestsBedWriter() {
-		delete m_pOss;
-		m_pOss = nullptr;
-		delete m_pWriter;
-		m_pWriter = nullptr;
+		delete m_pOssBed4;
+		m_pOssBed4 = nullptr;
+		delete m_pOssBed6;
+		m_pOssBed6 = nullptr;
 	}
-	ostringstream* m_pOss = nullptr;
-	uWriter* m_pWriter = nullptr;
+
+	ostringstream* m_pOssBed4 = nullptr;
+	ostringstream* m_pOssBed6 = nullptr;
 };
