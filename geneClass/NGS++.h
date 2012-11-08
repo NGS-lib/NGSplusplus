f971a95948024e1ec52026982a73209325c06d6b
anordell <alexei.nordell@usherbrooke.ca>
2012-11-08 14:20:28 -0500

    Removed old NGS++.h

--- geneClass/NGS++.h
+++
@@ -1,9 +0,0 @@
-/**< All the includes */
-#include "utility/utility.h"
-#include "IO/uToken.h"
-#include "IO/uHeader.h"
-#include "IO/Parser/uParser.h"
-#include "uRegion.h"
-#include "uFormats.h"
-#include "uGeneException.h"
-#include "uTags.h"
