fa6cc8f3c05c41580ecc78f935645773658b4623
anordell <alexei.nordell@usherbrooke.ca>
2012-11-08 14:19:08 -0500

    uWriterSam commit

---
+++ geneClass_testing/uWriter/tests_uWriterSam.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include <string>
+#include <sstream>
+#include "IO/Writer/uWriter.h"
+#include "tests_Writer_Fixtures.h"
+
+using namespace std;
+using namespace NGS;
+
+/*
+ * Tests for the function:
+ *		void writeToken(const uToken& token);
+ */
+TEST_F(TestSamWriter, WriteToken_ValidSam) {
+	string expected = "ab00001\t256	chr1\t1\t255\t2M3I2X1=12X\t*\t*\t0\tACGTN.acgtn.ACGTGTCN\t####################";
+
+	ASSERT_TRUE(m_pOssSAM->str().find(expected) != string::npos);
+}
