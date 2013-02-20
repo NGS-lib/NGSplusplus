006c4f248f02c7720c02974b575c948fb751f274
anordell <alexei.nordell@usherbrooke.ca>
2013-02-19 19:33:24 -0500

    Sample to cut a file into Chrom
    Sample cutting every duplicate in a file

---
+++ NGS/sample-src/splitToChrom/splitToChrom.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "NGS++.h"
+using namespace std;
+using namespace NGS;
+
+int main(int argc, char **argv)
+{
+    /**< Simple parameter validation. We heavily recommand you use a library to manage your command line parameters */
+    if (argc!=4)
+    {
+        cerr << "Please follow exactly the following signature"<<endl<<"fileToSplit"<<endl;
+        return 0;
+    }
+    ifstream firstStream;
+    {
+        string firstPath=argv[1];
+        try
+        {
+            /**< Load file in a corresponding stream. Will throw if path is invalid */
+            utility::loadStream(firstPath,firstStream);
+        }
+        catch(...)
+        {
+            /**< If invalid filePath, error message and abort. At this point, we have not validated the file format. */
+            cerr << "Error loading file. Check your path"<<endl;
+            return 0;
+        }
+    }
+
+    std::string type = argv[2];
+    std::string strID = argv[3];
+    if ((type!="BED")&&(type!="GFF")&&(type!="GTF")){
+         /**< If invalid filePath, error message and abort. At this point, we have not validated the file format. */
+            cerr << "Please elect type from BED/GFF/GTF"<<endl;
+            return 0;
+    }
+
+
+    /**< Catch block, for any number of possible errors. This will also catch Parser errors */
+  try {
+         /**< Declare our Parser */
+        uParser typeParser(&firstStream,type);
+        /**< Splitting a file by line is a very simple operation. Simpler to access the raw file without passing by an EXP structure */
+        while(!typeParser.eof())
+        {
+            uToken token =typeParser.getNextEntry();
+            std::string curChr= token.getParam(token_param::CHR);
+            ofstream curStream(curChr+"_"+strID,ios::out | ios::app );
+            curStream <<typeParser.getPreviousRaw()<<endl;
+        }
+    }
+    /**< Global exception handling. Practicalloy, this should probably be split so as to
+         manage parser errors seperately.
+     */
+    catch(ugene_exception_base & e)
+    {
+        cout << fetchStringError(e)<<endl;
+    }
+
+}
