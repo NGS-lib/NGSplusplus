9620418b40ec5aff3d2cb1e3c2f32e7e38f345bd
Charles Joly Beauparlant <charles.joly-beauparlant@crchul.ulaval.ca>
2013-03-26 10:21:42 -0400

    Added the tutorial file linkSample.cpp
    Signed-off-by: Charles Joly Beauparlant <charles.joly-beauparlant@crchul.ulaval.ca>

---
+++ NGS/sample-src/tutorial/linkSample.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "NGS++.h"
+
+using namespace std;
+using namespace NGS;
+
+int main() {
+        uGene dummyGene;
+	cout << "Looks like the linking worked correctly!" << endl;
+	return 0;
+}
