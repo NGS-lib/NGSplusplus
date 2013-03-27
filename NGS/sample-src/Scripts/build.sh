4d94f463c0041a16a77a70a589bf286fd8beb4ce
anordell <alexei.nordell@usherbrooke.ca>
2013-03-27 14:24:36 -0400

    Added -lz flag at appropriate position in build script

--- NGS/sample-src/Scripts/build.sh
+++ NGS/sample-src/Scripts/build.sh
@@ -3,4 +3,4 @@
 filename=$1
 output=bin/$(basename ${filename%.*})
 
-g++-4.7 -lz -O3 -Wall $filename -o $output -std=c++11 ../../libs/libNGS.a -I../../NGS
+g++-4.7 -O2 -Wall $filename -o $output -std=c++11 ../../libs/libNGS.a -I../../NGS -lz
